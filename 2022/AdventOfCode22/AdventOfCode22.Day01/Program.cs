b5c161a0727f1c40cdb60108d6b0238411d980a4
Tom Blomfield <tom.blomfield@outlook.com>
2022-12-01 21:48:24 +1300

    Add day 01

--- 2022/AdventOfCode22/AdventOfCode22.Day01/Program.cs
+++ 2022/AdventOfCode22/AdventOfCode22.Day01/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿StreamReader reader = new("input.txt");
+
+string input = reader.ReadToEnd();
+
+string[] inventories = input.Split(Environment.NewLine + Environment.NewLine);
+
+List<int> inventoryTotals = inventories
+    .Select(x => x.Split(Environment.NewLine).Sum(int.Parse))
+    .OrderByDescending(x => x)
+    .ToList();
+
+Console.WriteLine("Day One:");
+
+Console.WriteLine($"Largest Total is: {inventoryTotals.First()}");
+
+Console.WriteLine($"Top 3 totals are: {inventoryTotals.Take(3).Sum()}");
